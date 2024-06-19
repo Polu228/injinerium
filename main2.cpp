362b240b374105bbff4fdd976ce24a50cf7049fe
Polu228 <79249264+Polu228@users.noreply.github.com>
2024-06-20 01:03:04 +0500

    Изменены некоторые слова в кавычках

---
+++ main2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, user!" << std::endl;
+
+    int a, b;
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+
+    int sum = a + b;
+    std::cout << "The result is: " << sum << std::endl;
+
+    return 0;
+}
