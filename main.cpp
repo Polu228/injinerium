86ca66bf095e9dd1aefc8422b1d1b3fe040a91a1
Polu228 <79249264+Polu228@users.noreply.github.com>
2024-06-21 15:50:57 +0500

    Новый вывод текст

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, user!" << std::endl;
 
     int a, b;
     std::cout << "Enter two numbers: ";
     std::cin >> a >> b;
 
     int sum = a + b;
-    std::cout << "The sum is: " << sum << std::endl;
+    std::cout << "The result is: " << sum << std::endl;
 
     return 0;
-}
+} 
